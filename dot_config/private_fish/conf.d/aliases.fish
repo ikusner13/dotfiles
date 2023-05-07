21e8d0d8bfbeec6bd4ea7369d3c6f20d154d5cb6
Ian Kusner <ikusner13@gmail.com>
2023-05-06 23:29:25 -0400

    fish and nvmim

---
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,16 @@
+alias fish-config='cd ~/.config/fish/'
+alias nv='nvim'
+alias lg='lazygit'
+alias fishalias='nvim ~./config/fish/conf.d/aliases.fish'
+
+alias ls='exa --group-directories-first'
+alias ll=' ls --git -l'
+alias cat=' bat'
+
+# projects
+alias tldw='cd ~/dev/tldw'
+
+# pnpm
+alias pdev='pnpm dev'
+alias pi='pnpm install'
+
